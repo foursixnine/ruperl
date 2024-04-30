fb5856e7f8139aa5f20cf667b210f7d7d28c587f
Santiago Zarate <santiago@zarate.co>
2024-05-01 00:32:42 +0200

    Initial version finally working

--- build.rs
+++ build.rs
@@ -1,9 +1,58 @@
+extern crate bindgen;
+extern crate cc;
+
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
-    println!("cargo:rustc-link-search=/opt/homebrew/opt/perl/lib/perl5/5.38/darwin-thread-multi-2level/CORE");
-    println!("cargo:rustc-link-lib=perl");
+    // Tell cargo to tell rustc to link the system perl
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+
+    // println!("cargo:rerun-if-changed=perl-boilerplate/wrapper.h");
+    // generate the perlxsi.c file
+    Command::new("perl")
+        .args(&["-MExtUtils::Embed", "-e", "xsinit", "--", "-o", "perlxsi.c"])
+        .status()
+        .expect("Failed to generate perlxsi.c");
+
+    // generate the ccopts (c flags) from perl
+    let ccopts = Command::new("perl")
+        .args(&["-MExtUtils::Embed", "-e", "ccopts"])
+        .output()
+        .expect("Failed to get ccopts")
+        .stdout;
+    let ccopts = String::from_utf8_lossy(&ccopts).trim().to_string();
+    let ccopts_iter = ccopts.split_whitespace();
+
+    // Get ldopts from Perl and add them to the linker flags
+    let ldopts = Command::new("perl")
+        .args(&["-MExtUtils::Embed", "-e", "ldopts"])
+        .output()
+        .expect("Failed to get ldopts")
+        .stdout;
+    let ldopts_command = String::from_utf8_lossy(&ldopts);
+    let ldopts_iter = ldopts_command.split_whitespace();
+
+    // Tell cargo to link the perlxsi object file
+    for opt in ldopts_iter {
+        println!("cargo:rustc-link-arg={}", opt);
+    }
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+
+    // Compile perlxsi.c with ccopts
+    let mut build = cc::Build::new();
+    build.file("perlxsi.c");
+    for opt in ccopts_iter {
+        build.flag(opt);
+    }
+    build.compile("perlxsi");
+
+    // println!("cargo:rustc-link-lib=static=perlxsi");
+    // println!("cargo:rustc-link-search=native={}", out_dir.display());
+
+    // let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+    // let perlxsi_a = out_dir.join("libperlxsi.a");
 
     let bindings = bindgen::Builder::default()
         .header("perl-boilerplate/wrapper.h")
@@ -13,8 +62,8 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
